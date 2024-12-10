34a7bd21df49a966f2ae9c0571bb714c688d73cc
Marco T. Morazan <morazanm@gmail.com>
2024-12-10 16:50:33 -0500

    Update to include generate-symbol in eopl-extras

--- eopl-extras.rkt
+++ eopl-extras.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-(provide ormap andmap build-list foldr foldl add1 sub1 first second third
-         fourth fifth sixth seventh eighth ninth tenth displayln display
+(provide filter ormap andmap build-list foldr foldl add1 sub1 first second third
+         fourth fifth sixth seventh eighth ninth tenth rest displayln display
          vector-of for-each false? empty? take drop stream-cons stream-ref
-         stream-map λ format)
+         stream-map λ void format generate-symbol last drop-right append-map)
 
 (define (vector-of V f)
   (define (helper low high)
@@ -12,3 +12,9 @@
         (and (f (vector-ref V low))
              (helper (add1 low) high))))
   (helper 0 (sub1 (vector-length V))))
+
+;; symbol --> symbol
+;; Purpose: Generate a new symbol
+(define (generate-symbol s)
+  (let ((new-symb (string->symbol (symbol->string (gensym (string->symbol (string-append (symbol->string s) "-")))))))
+    new-symb))
